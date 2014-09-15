fcf47f93e0323874f535172a685397730d8d8210
Drew Errington <drew@errington.xyz>
2014-09-15 13:31:52 -0400

    Modified for basic web utility Reads uploaded file, outputs basic information

---
+++ tie_golf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import cgi, os
+import cgitb; cgitb.enable()
+import tfr_reader as TFR
+
+form = cgi.FieldStorage()
+
+# Get filename here.
+fileitem = form['file']
+pilot = fileitem.file
+
+score = TFR.plong(pilot,4)
+lasersfired = TFR.plong(pilot,1908)
+warheadsfired = TFR.pword(pilot,1920)
+laserhits = TFR.plong(pilot,1912)
+warheadhits = TFR.plong(pilot,1922)
+laserless = score - (laserhits * 3)
+golf = score + (lasersfired * 2) + (warheadsfired * 100) - (laserhits * 3) - (warheadhits * 100)
+
+#data = fileitem[2]
+
+print("""\
+Content-Type: text/html\n
+<html>
+<body>
+""")
+
+print('Score : ',score,'<br>\n')
+print('Lasers Fired : ',lasersfired,'<br>\n')
+print('Warheads Fired : ',warheadsfired,'<br>\n')
+print('Lasers Hit : ',laserhits,'<br>\n')
+print('Warheads Hit : ',warheadhits,'<br>\n')
+print('Laserless Score : ',laserless,'<br>\n')
+print('Golf Score : ',golf,'<br>\n')
+
+print("""\
+</body>
+</html>
+""")
