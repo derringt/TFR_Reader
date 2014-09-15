0f7b3f7dcb0cf23ce141c6df363548ac50005edb
Drew Errington <drew@errington.xyz>
2014-09-15 13:36:12 -0400

    Revert "Revert "Modified for basic web utility""
    This reverts commit 3d677ca3279e29a219450f91d965ddb8298915ad.

--- tfr_reader.py
+++ tfr_reader.py
@@ -2,8 +2,11 @@ import os
 import io
 from struct import unpack
 
+<<<<<<< HEAD
+=======
 ranks = ['Cadet', 'Officer', 'Lieutenant', 'Captain', 'Commander', 'General']
 
+>>>>>>> f821efc6b203702f58b4ee034a676935e023269c
 def pbyte(file, pos):
     file.seek(pos)
     return unpack('B',file.read(1))[0]
@@ -15,6 +18,8 @@ def pword(file, pos):
 def plong(file, pos):
     file.seek(pos)
     return unpack('<L', file.read(4))[0]
+<<<<<<< HEAD
+=======
 
 file = 'test.tfr'
 pilot = open(file, 'rb')
@@ -26,3 +31,4 @@ rank = pbyte(pilot, 2)
 print ranks[rank]
 
 pilot.close()
+>>>>>>> f821efc6b203702f58b4ee034a676935e023269c
